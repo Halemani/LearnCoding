02c4d302b7152b58a191fa1e7ea0da2c2636ecee
halemani <santosh.halemani@gmail.com>
2019-11-19 14:40:09 +0530

    Bubble sort algo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here man
+        int[] myArray = {10, -45, 15, 2, 3, -3, 79};
+        for(int unSortedIndex=myArray.length-1; unSortedIndex>0; unSortedIndex--){
+            for(int i=0; i<unSortedIndex; i++){
+                if(myArray[i] > myArray[i+1]){
+                    int temp = myArray[i];
+                    myArray[i] = myArray[i+1];
+                    myArray[i+1] = temp;
+                }
+            }
+        }
+
+        for(int i=0; i < myArray.length; i++){
+            System.out.println(myArray[i]);
+        }
     }
 }
